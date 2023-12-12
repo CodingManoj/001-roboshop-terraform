18db4644e9bd28ebc3a1c579c3eb8ab2b4b5ad77
CodingManoj <CodingManoj@gmail.com>
2023-12-11 23:43:46 -0500

    Jenkins-Shared-Library Auto Push

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 # Looping Moudles is the best thing, rather resources
 
 module "instances" {
-    for_each           = var.components
-   
     source             = "git::https://github.com/CodingManoj/001-tf-module-test.git"
+    
+    for_each           = var.components
+    
     instance_type      = each.value["instance_type"]
     name               = each.value["name"]
     sgid               = var.sgid
