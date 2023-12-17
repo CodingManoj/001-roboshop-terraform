62cc41b88842e250f10653e710c8bc1983af7bca
CodingManoj <CodingManoj@gmail.com>
2023-12-17 16:15:39 -0500

    Terraform Auto Push

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@
 
 # Creates VPC
 module "components" {
-    source      = "git::https://github.com/CodingManoj/001-tf-module-test.git"
+    source      = "git::https://github.com/CodingManoj/001-tf-module-vpc.git"
     
     for_each    = var.vpc
     cidr_block  = each.value["cidr"]
